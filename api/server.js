dfb26f1ff9b9516d33c930a51e00dbda586a2f16
Roeck <rons9n@gmail.com>
2021-07-14 19:30:38 -0400

    Delete and put request methods

--- api/server.js
+++ api/server.js
@@ -30,4 +30,20 @@ app.post('/todo/new',(req, res) => {
     res.json(todo);
 });
 
+app.delete('/todo/delete/:id',async (req,res) => {
+    const result=await Todo.findByIdAndDelete(req.params.id);
+
+    res.json(result);
+})
+
+app.put('/todo/complete/:id',async (req,res) => {
+    const todo=await Todo.findById(req.params.id);
+
+    todo.complete = !todo.complete;
+
+    todo.save();
+
+    res.json(todo);
+})
+
 app.listen(3001, () => console.log("Running on port 3001"));
